5104616365fba76255621a6807aeb4aa0f557fc1
JeeWaa <jeewa228@gmail.com>
2022-10-09 15:39:19 +0530

    sign in

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import LoginPage from './screens/LoginPage'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import SignInPage from './screens/SignInPage';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="SignInPage" component={SignInPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- screens/SignInPage.js
+++ screens/SignInPage.js
@@ -2,14 +2,18 @@ import { Text, StyleSheet } from 'react-native'
 import { React, useState } from 'react'
 import { NativeBaseProvider, Center, Stack, Input, Button, AspectRatio, Image } from 'native-base'
 
-export default function LoginPage({navigation}) {
+export default function SignInPage({navigation}) {
 
+    const [name,setName] = useState("");
     const [username,setUsername] = useState("");
     const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
 
     const printStates=()=>{
+        console.log(name);
         console.log(username);
         console.log(password);
+        console.log(email);
     }
 
   return (
@@ -19,14 +23,20 @@ export default function LoginPage({navigation}) {
           <Center>
             {/* <Text style={styles.text}>Login</Text> */}
             <AspectRatio ratio={{base: 3 / 4,md: 9 / 10
-              }} height={{base: 250,md: 400
+              }} height={{base: 200,md: 400
             }}>
               <Image resizeMode="cover" source={{
-                uri: "https://www.origastock.com/images/3d-characters/download3/6.png"
+                uri: "https://www.origastock.com/images/3d-characters/download3/16.png"
               }} alt="Picture of a Flower" />
             </AspectRatio>
           </Center>
 
+          <Input 
+            variant="rounded" 
+            placeholder="Name" 
+            value={name}
+            onChangeText={(e)=>{setName(e)}}
+          />
           <Input 
             variant="rounded" 
             placeholder="Username" 
@@ -40,12 +50,18 @@ export default function LoginPage({navigation}) {
             value={password}
             onChangeText={(e)=>{setPassword(e)}}
           />
+          <Input 
+            variant="rounded" 
+            placeholder="Email"
+            value={email}
+            onChangeText={(e)=>{setEmail(e)}}
+          />
           <Button style={styles.button} size="lg" colorScheme="blue" onPress={printStates}>
-            Login
+            Sign In
           </Button>
 
           <Center>
-            <Text style={styles.sign} onPress={() => navigation.navigate('SignInPage')}>Sign In</Text>
+            <Text style={styles.login} onPress={() => navigation.navigate('LoginPage')}>Login</Text>
           </Center>
           
         </Stack>
@@ -60,14 +76,14 @@ const styles = StyleSheet.create({
     //     fontSize: 40,
     //     paddingBottom: 20
     // },
-    sign: {
+    login: {
       color: 'blue',
         fontSize: 12,
-        paddingTop: 40,
+        paddingTop: 10,
         paddingBottom: 30
     },
     button: {
       borderRadius: 20,
-      marginTop: 40
+      marginTop: 20
     }
   });
